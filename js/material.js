4f36b0cea78190b7cb348cf5d28ef64f1a4f12a3
Mette <481695@student.fontys.nl>
2022-04-07 16:30:07 +0200

    Textbox label error gefixt, design tot nu toe responsive gemaakt

--- js/material.js
+++ js/material.js
@@ -1,6 +1,8 @@
-mdc.ripple.MDCRipple.attachTo(document.querySelector('.foo-button'));
-import {MDCTextField} from '@material/textfield';
+// import {MDCTextField} from '@material/textfield';
 import {MDCRipple} from '@material/ripple';
 
-new MDCTextField(document.querySelector('.message-field'));
+// new MDCTextField(document.querySelector('.message-field'));
 new MDCRipple(document.querySelector('.send-button'));
+
+const textField = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field'));
+const helperText = new mdc.textField.MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
