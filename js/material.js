b1e03081723ff818b16ac491bbfdea73291c4204
Mette <481695@student.fontys.nl>
2022-04-07 16:53:20 +0200

    Footer gemaakt, kleine foutjes opgeschoond

--- js/material.js
+++
@@ -1,8 +0,0 @@
-// import {MDCTextField} from '@material/textfield';
-import {MDCRipple} from '@material/ripple';
-
-// new MDCTextField(document.querySelector('.message-field'));
-new MDCRipple(document.querySelector('.send-button'));
-
-const textField = new mdc.textField.MDCTextField(document.querySelector('.mdc-text-field'));
-const helperText = new mdc.textField.MDCTextFieldHelperText(document.querySelector('.mdc-text-field-helper-text'));
