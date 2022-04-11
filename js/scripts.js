7b52ef0d570b58285ff3c2344c14e758a4fa4580
Mette <481695@student.fontys.nl>
2022-04-11 11:27:07 +0200

    Password visibility button verandert van icoon wanneer password visible wordt.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,14 @@
 const showPasswordButton = document.getElementById("showPasswordButton");
+const visibilityIcon = document.getElementById("visibility-icon");
 showPasswordButton.addEventListener("click",showPassword);
 function showPassword() {
     let x = document.getElementById("password");
     if (x.type === "password") {
         x.type = "text";
+        visibilityIcon.textContent = 'visibility';
     } else {
         x.type = "password";
+        visibilityIcon.textContent = 'visibility_off';
     }
 }
 
