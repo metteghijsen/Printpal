63060a67b514b7033c505f23f6db2a98fdb8643c
Maarten Laptop <441782@student.fontys.nl>
2022-04-15 10:26:03 +0200

    comments to code

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+/**
+ * Registers the service worker.           
+ */
 if("serviceWorker" in navigator){
     navigator.serviceWorker.register("service-worker.js").then(function(registering){
         // Registration was successful
@@ -34,6 +37,7 @@ import {
     update
   }
   from 'https://www.gstatic.com/firebasejs/9.1.1/firebase-database.js';
+
   const firebaseConfig = {
     apiKey: "AIzaSyBOAn8uYKrk80TdpijN8gI8GYAyBfZ0qrw",
     authDomain: "printpal-fcd92.firebaseapp.com",
@@ -41,21 +45,25 @@ import {
     storageBucket: "printpal-fcd92.appspot.com",
     messagingSenderId: "4029039652",
     appId: "1:4029039652:web:4a023c24debd05c21004b1",
-    // measurementId: "G-KMWRY5M814",
     databaseURL: "https://printpal-fcd92-default-rtdb.firebaseio.com/"
   };
   
-  // Initialize Firebase
+
+  /**
+   * Initializes the Firebase app and returns the analytics and database objects.       
+   * @param {firebase.Config} firebaseConfig - the Firebase configuration object.       
+   * @returns {firebase.analytics.Analytics} - the analytics object.       
+   * @returns {firebase.database.Database} - the database object.       
+   */
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
-    const database = getDatabase(app);
-  
-
- /*Function for writing data to database*/
+  const database = getDatabase(app);
     
+    /**
+     * Writes the user's message to the database.       
+     * @param {string} message - the message to write to the database.       
+     */
     function writeUserData(message) {
-        console.log(database);
-        // userId = document.getElementById('userId').value;;
         message = document.getElementById('messageForm').value;
         set(ref(database, 'messages/' + message), {
           anonymousmessage: message,
@@ -63,6 +71,10 @@ import {
         console.log("Done");
       }
       
+
+      /**
+       * Adds a listener to the send button that writes the user's data to the database.       
+       */
       window.addEventListener('DOMContentLoaded', (event) => {
         console.log('DOM fully loaded and parsed');
         document.getElementById("sendbutton").addEventListener("click", e => {
