1ae2619e497f4aef3d64d8862f736259eb34e29b
Mette <481695@student.fontys.nl>
2022-05-31 12:10:28 +0200

    Preferences scherm afgemaakt.

--- js/preferences.js
+++ js/preferences.js
@@ -1,7 +1,9 @@
 let categoryItem1 = document.getElementById("category-item-1");
 let categoryItem2 = document.getElementById("category-item-2");
 let categoryItem3 = document.getElementById("category-item-3");
-let categoryArray = [categoryItem1,categoryItem2,categoryItem3];
+let categoryItem4 = document.getElementById("category-item-4");
+let categoryItem5 = document.getElementById("category-item-5");
+let categoryArray = [categoryItem1,categoryItem2,categoryItem3,categoryItem4,categoryItem5];
 
 categoryArray.forEach(category=> {
     category.addEventListener("click", () => {
